8e26a349de01e68b5e98b21c24413d5c4965e3be
Keiran Smith <affix@affix.me>
2013-06-18 22:33:29 +0100

    Updates

--- GoldList/DetailViewController.h
+++ GoldList/DetailViewController.h
@@ -1,10 +1,21 @@
 //
 //  DetailViewController.h
-//  GoldList
 //
-//  Created by Keiran Smith on 28/10/2012.
-//  Copyright (c) 2012 Keiran Smith. All rights reserved.
+//  Created by Keiran Smith on 27/10/2012.
+//  Copyright (c) 2013 Keiran Smith. All rights reserved.
+//  This file is part of GoldList.
 //
+//  GoldList is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  GoldList is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
 
 #import <UIKit/UIKit.h>
 #import "globalVar.h"
@@ -18,5 +29,12 @@
 @property (weak, nonatomic) IBOutlet UILabel *connection;
 @property (weak, nonatomic) IBOutlet UILabel *gold;
 @property (weak, nonatomic) IBOutlet UILabel *players;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *modeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *connectionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *playerLabel;
+
 
 @end
--- GoldList/DetailViewController.m
+++ GoldList/DetailViewController.m
@@ -1,10 +1,21 @@
 //
 //  DetailViewController.m
-//  GoldList
 //
-//  Created by Keiran Smith on 28/10/2012.
-//  Copyright (c) 2012 Keiran Smith. All rights reserved.
+//  Created by Keiran Smith on 27/10/2012.
+//  Copyright (c) 2013 Keiran Smith. All rights reserved.
+//  This file is part of GoldList.
 //
+//  GoldList is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  GoldList is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
 
 #import "DetailViewController.h"
 
@@ -30,20 +41,34 @@
     [[self navigationItem] setTitle:serverName];
     NSLog(@"%@", serverGold);
     
+    /* Font Setups :) */
+    [_playerLabel setFont:[UIFont fontWithName:@"uni 05_53" size:20]];
+    [_descriptionLabel setFont:[UIFont fontWithName:@"uni 05_53" size:20]];
+    [_modeLabel setFont:[UIFont fontWithName:@"uni 05_53" size:20]];
+    [_connectionLabel setFont:[UIFont fontWithName:@"uni 05_53" size:20]];
+    
+    [_gold setFont:[UIFont fontWithName:@"uni 05_53" size:40]];
+    [_mode setFont:[UIFont fontWithName:@"uni 05_53" size:15]];
+    [_connection setFont:[UIFont fontWithName:@"uni 05_53" size:15]];
+    [_description setFont:[UIFont fontWithName:@"uni 05_53" size:15]];
+    [_players setFont:[UIFont fontWithName:@"uni 05_53" size:15]];
+    
     if([serverGold intValue] != 0)
     {
         [_gold setText:@"GOLD"];
+        [_gold setTextColor:[UIColor yellowColor]];
     }
     else
     {
         [_gold setText:@"NOT GOLD"];
+        [_gold setTextColor:[UIColor redColor]];
     }
+    
     [_connection setText:[NSString stringWithFormat:@"%@:%@", serverv4, serverPort]];
     [_mode setText:serverMode];
     [_description setText:serverDescription];
     [_players setText:[NSString stringWithFormat:@"%@/%@", serverCurrent, serverMax]];
     
-	// Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning
